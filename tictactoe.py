406c010aad3f9f194239daf9b7d4709e6c6d3c4c
Jediah Logiodice <jediah@logiodice.com>
2017-11-27 22:26:41 -0500

    Added method for updating grid, added color to print grid, and finalized functions to collecting user input

--- tictactoe.py
+++ tictactoe.py
@@ -1,12 +1,38 @@
 import os
+from sys import platform
+
+graph = [None, None, None]
+graph[0] = [i for i in range(1, 4)]
+graph[1] = [i for i in range(4, 7)]
+graph[2] = [i for i in range(7, 10)]
 
 
-def print_grid():
-    os.system('cls')
-    print("\t\t\t_{1}_ _{2}_ _{3}_")
-    print("\t\t\t_{4}_|_{5}_|_{6}_")
-    print("\t\t\t_{7}_|_{8}_|_{9}_")
-    print('')
+class BColors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def print_grid(clear=True):
+    if clear:
+        if platform == "linux" or platform == "linux2":
+            os.system('clear')
+        elif platform == 'win32':
+            os.system('cls')
+        elif platform == 'darwin':
+            pass
+    else:
+        print()
+
+    print("\t\t\t_{%s}_|_{%s}_|_{%s}_" % (graph[0][0], graph[0][1], graph[0][2]))
+    print("\t\t\t_{%s}_|_{%s}_|_{%s}_" % (graph[1][0], graph[1][1], graph[1][2]))
+    print("\t\t\t_{%s}_|_{%s}_|_{%s}_" % (graph[2][0], graph[2][1], graph[2][2]))
+    print()
 
 
 def print_instructions(players):
@@ -14,14 +40,32 @@ def print_instructions(players):
     print("%s is [X], %s is [O]. Each player will take a turn." % (players[0], players[1]))
     print("The Tic Tac Toe board is a 3x3 square, it looks like this:")
     print("The game is broken up into a grid pattern, as follows:")
-    print('')
-    print("\t\t\t_{1}_ _{2}_ _{3}_")
-    print("\t\t\t_{4}_|_{5}_|_{6}_")
-    print("\t\t\t_{7}_|_{8}_|_{9}_")
-    print('')
+    print_grid(False)
     print("To place your mark, just type a number [1-9]")
     print('*'*60)
-    response = input("Press any key to continue: ")
+    print("Press any key to continue: ")
+    input()
+
+
+def set_mark(index, value):
+    # print("Player %s chose %s" % (index, value))
+    internal_index = 0
+    if index in range(1, 4):
+        graph_index = 0
+        internal_index = index - 1
+    elif index in range(4, 7):
+        graph_index = 1
+        internal_index = index - 4
+    else:
+        graph_index = 2
+        internal_index = index - 7
+
+    if graph[graph_index][internal_index] != index:
+        print("This box has already been selected, please select another")
+        return False
+    else:
+        graph[graph_index][internal_index] = value
+        return True
 
 
 def main():
@@ -44,26 +88,32 @@ def main():
         # Loop until player puts in a valid number 1-9
         while not successful:
             # Even mod %2 = 0
-            player = players[n % 2]
+            player_index = n % 2
+            player_mark = BColors.OKGREEN + 'X' + BColors.ENDC \
+                if player_index == 0 \
+                else BColors.FAIL + 'O' + BColors.ENDC
 
             # Loop until they put in a number
             is_number = False
             while not is_number:
                 print_grid()
                 try:
-                    player_move = int(input('%s, select your move [1-9] : ' % player))
+                    player_move = int(input('%s, select your move [1-9] : ' % players[player_index]))
                     is_number = True
                 except ValueError:
                     is_number = False
 
             # Set successful = False if they didn't chose 1-9
             if player_move in range(1, 10):
-                successful = True
+                # The player chose a range 1-9, now let's see if we can successfully mark this selection
+                successful = set_mark(player_move, player_mark)
 
-        # Replace the { Num }
         # Check to see if a player has won
         # move to the next
+
         pass
+
+    print_grid()
 
 
 main()
